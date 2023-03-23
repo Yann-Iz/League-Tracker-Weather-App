f338831efd8dd21846017ecdb62a97cb0bc7a4c2
Yann Izeghlouche <yannizeghlouche@MacBook-Air-de-Yann.local>
2023-03-23 11:33:38 +0100

    Weather App Commit

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Home from "./component/Home";
+import Rotation from "./component/Rotation";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "./component/Login";
+
+const tab = createBottomTabNavigator();
+const stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <stack.Navigator initialRouteName="Login">
+        <stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <stack.Screen
+          name="NavBotton"
+          component={NavBottom}
+          options={{ headerShown: false }}
+        />
+      </stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+const NavBottom = () => {
+  return (
+    <tab.Navigator>
+      <tab.Screen
+        name="Home"
+        component={Home}
+        options={{ headerShown: false }}
+      ></tab.Screen>
+      <tab.Screen
+        name="Rotation"
+        component={Rotation}
+        options={{ headerShown: false }}
+      ></tab.Screen>
+    </tab.Navigator>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
